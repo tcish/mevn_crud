0e5b5b84f2094f62636bdbf34dbfc8244ce1c467
tcish <khaladkhan250@gmail.com>
2022-10-14 22:23:19 +0600

    structured folder & added cros, axios

--- api/server.js
+++ api/server.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: "/.env" });
 const app = require("./app");
-
 mongoose
-  .connect(process.env.DATABASE, {
-   useNewUrlParser: true,
+  .connect("mongodb://localhost:27017/nodejs_crud", {
+    useNewUrlParser: true,
   })
   .then((con) => {
     console.log("DB Connected");
--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import Antd from "ant-design-vue";
 import App from "./App.vue";
+import "ant-design-vue/dist/antd.css";
 import router from "./router";
 
-import VueFormulate from "@braid/vue-formulate";
-
-createApp(App).use(router, VueFormulate, options).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.use(Antd);
+app.mount("#app");
